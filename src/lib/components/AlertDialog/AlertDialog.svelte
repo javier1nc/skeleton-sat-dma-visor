62998b4ce4faea5e774ef0958b114faffa68b43d
Javier Sánchez Mendoza <code@javier1nc.com>
2025-09-04 17:33:35 -0600

    feat: DATOS INFORMATIVOS

---
+++ src/lib/components/AlertDialog/AlertDialog.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { AlertDialog } from 'bits-ui';
+	export let open!: boolean;
+	export let title = 'Confirm';
+	export let description = '';
+</script>
+
+<AlertDialog.Root bind:open>
+	<AlertDialog.Trigger class="hidden">Open</AlertDialog.Trigger>
+	<AlertDialog.Portal>
+		<AlertDialog.Overlay class="fixed inset-0 bg-black/50" />
+		<AlertDialog.Content
+			class="fixed top-1/2 left-1/2 z-50 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-6 shadow-lg"
+		>
+			<AlertDialog.Title class="block font-semibold text-[#621132]">{title}</AlertDialog.Title>
+			<AlertDialog.Description class="text-black">{description}</AlertDialog.Description>
+			<div class="mt-4 flex justify-end space-x-2">
+				<AlertDialog.Cancel
+					class="w-full rounded bg-[#621132] px-6 py-2 text-white shadow hover:bg-[#4e0f28] sm:w-auto"
+					>NO</AlertDialog.Cancel
+				>
+				<AlertDialog.Action
+					class="w-full rounded bg-[#621132] px-6 py-2 text-white shadow hover:bg-[#4e0f28] sm:w-auto"
+					>SI</AlertDialog.Action
+				>
+			</div>
+		</AlertDialog.Content>
+	</AlertDialog.Portal>
+</AlertDialog.Root>
--- src/lib/components/Form.svelte
+++ src/lib/components/Form.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 	import { PUBLIC_BASE_PATH } from '$env/static/public';
 	import { PUBLIC_APP_PATH } from '$env/static/public';
+
+	import { TimerReset as Reset } from '@lucide/svelte';
+
+	import AlertDialog from '$lib/components/AlertDialog/AlertDialog.svelte';
+
+	let showAlert220610 = false;
+	let Alert220610Answered = false;
+
+	let showAlert220619 = false;
+	let Alert220619Answered = false;
+
 	const basePath = PUBLIC_BASE_PATH;
 	const appPath = PUBLIC_APP_PATH;
 
@@ -21,6 +32,30 @@
 		{ position: '3', name: 'Radon', symbol: 'Rn', atomic_no: '86' },
 		{ position: '4', name: 'Technetium', symbol: 'Tc', atomic_no: '43' }
 	];
+
+	function handleFocus220610() {
+		console.log('handleFocus220610');
+
+		if (!Alert220610Answered) {
+			showAlert220610 = true;
+			Alert220610Answered = true;
+		} else {
+			showAlert220610 = false;
+			Alert220610Answered = false;
+		}
+	}
+
+	function handleFocus220619() {
+		console.log('handleFocus220619');
+
+		if (!Alert220619Answered) {
+			showAlert220610 = true;
+			Alert220610Answered = true;
+		} else {
+			showAlert220619 = false;
+			Alert220619Answered = false;
+		}
+	}
 </script>
 
 <div class="space-y-2">
@@ -38,7 +73,7 @@
 					<span class="block font-semibold text-[#621132]"
 						>220608 - INSTITUCIÓN FINANCIERA EN LA QUE SE RETORNA LA INVERSIÓN</span
 					>
-					<!-- Institución Financiera -->
+					<!-- 220608 - INSTITUCIÓN FINANCIERA EN LA QUE SE RETORNA LA INVERSIÓN -->
 					<select class="select text-[#121132]">
 						<option value="1">Institución Financiera 1</option>
 						<option value="2">Institución Financiera 2</option>
@@ -48,6 +83,7 @@
 					</select>
 				</label>
 
+				<!-- 220609 - NÚMERO DE CUENTA DE INVERSIÓN O DE CONTRATO -->
 				<label class="block">
 					<span class="block font-semibold text-[#621132]"
 						>220609 - NÚMERO DE CUENTA DE INVERSIÓN O DE CONTRATO</span
@@ -58,16 +94,98 @@
 						maxlength="14"
 						pattern="[a-zA-Z0-9]+"
 						class="input p-1 text-black"
-						placeholder="ejemplo: 111100001111000011"
+						placeholder="ejemplo: A1110000111100001Z"
 					/>
 					<small class="text-sm text-gray-500"
 						>Se deberá anotar el número de cuenta de inversión o de contrato de la institución
 						financiera donde se retorna la inversión</small
 					>
 				</label>
+
+				<!-- 220610 - RECARGOS CAUSADOS A LA FECHA DEL RETORNO DE LA INVERSIÓN -->
+				<label class="block">
+					<span class="block font-semibold text-[#621132]"
+						>220610 - RECARGOS CAUSADOS A LA FECHA DEL RETORNO DE LA INVERSIÓN</span
+					>
+
+					<div class="flex">
+						<input
+							type="text"
+							maxlength="14"
+							pattern="[0-9]+"
+							class="input p-1 text-black"
+							placeholder="ejemplo: 111100001111000011"
+							on:focus={handleFocus220610}
+						/>
+					</div>
+
+					<small class="text-sm text-gray-500"
+						>Hacer uso de este beneficio implica que sus datos (nombre, denominación o razón social,
+						clave del RFC y el monto de los recargos causados que no pagará) sean publicados en el
+						Portal del SAT</small
+					>
+				</label>
+
+				<!-- 220619 - RECARGOS CAUSADOS A LA FECHA DEL RETORNO DE LA INVERSIÓN -->
+				<label class="block">
+					<span class="block font-semibold text-[#621132]"
+						>220619 - ACTUALIZACIÓN CAUSADA A LA FECHA DEL RETORNO DE LA INVERSIÓN</span
+					>
+
+					<div class="flex">
+						<input
+							type="text"
+							maxlength="14"
+							pattern="[0-9]+"
+							class="input p-1 text-black"
+							placeholder="ejemplo: 111100001111000011"
+							on:focus={handleFocus220619}
+						/>
+					</div>
+
+					<small class="text-sm text-gray-500"
+						>MONTO DE LA ACTUALIZACIÓN CAUSADA A LA FECHA DE RETORNO QUE NO PAGARÁ POR HACER USO DEL
+						BENEFICIO</small
+					>
+				</label>
+
 				<hr class="hr" />
 			</article>
 			<button class="btn w-full preset-filled-primary-500 text-white">Enviar</button>
 		</form>
 	</div>
 </div>
+
+<AlertDialog
+	bind:open={showAlert220610}
+	title="220610 - RECARGOS CAUSADOS A LA FECHA DEL RETORNO DE LA INVERSIÓN"
+	description="Hacer uso de este beneficio implica que sus datos (nombre, denominación o razón social, clave del RFC y el monto de los recargos causados que no pagará) sean publicados en el Portal del SAT.
+
+Si no desea ser publicado, puede optar por hacer el pago de dichos accesorios.
+
+¿Desea pagar los recargos?"
+	on:action={() => {
+		// logic here
+	}}
+	on:cancel={() => {
+		// logic here
+		showAlert220610 = false;
+	}}
+/>
+
+<AlertDialog
+	bind:open={showAlert220619}
+	title="220619 - ACTUALIZACIÓN CAUSADA A LA FECHA DEL RETORNO DE LA INVERSIÓN"
+	description="Hacer uso de este beneficio implica que sus datos (nombre, denominación o razón social, clave del RFC y el monto de la actualización causada que no pagará) sean publicados en el Portal del SAT.
+
+Si no desea ser publicado, puede optar por hacer el pago.
+
+¿Desea pagar la actualización?   SI   NO"
+	on:action={() => {
+		// logic here
+	}}
+	on:cancel={() => {
+		// logic here
+		showAlert220619 = false;
+	}}
+/>
